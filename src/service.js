d616b2c784ff832469536e535073242304e57370
Eray Arslan <relfishere@gmail.com>
2017-05-03 13:34:44 +0300

    service.js reformat

--- src/service.js
+++ src/service.js
@@ -1,38 +1,32 @@
 var service = {
-  worker: function (str) {
-    return new Worker(URL.createObjectURL(new Blob([str.pass(Utils.guid())], {type: "text/javascript"})));
-  },
-  navigate: function (url) {
-    chrome.tabs.create({url: url});
-  },
-  moneyToNumber: function (str) {
-    var a = str.indexOf(DEFAULT_CURRENCY) > -1 ? str.split('.').join('') : str;
-    return parseFloat(a);
-  },
   init: function () {
+    this.guid = Utils.guid();
     this.db = new PouchDB('hi');
     this.session = new Session();
-
-    this.worker(worker)['onmessage'] = function (e) {
-      console.log(e.data);
-    };
-
+    this.initSession();
+    this.worker = new Worker(this.getBlobScriptUrl(worker.pass(this.guid)));
+    this.worker.onmessage = this.onWorker.bind(this);
+    this.defineEvents();
+  },
+  defineEvents: function () {
+    chrome.browserAction.onClicked.addListener(this.onClickedIcon.bind(this));
+    chrome.runtime.onMessage.addListener(this.onMessage.bind(this));
+  },
+  initSession: function () {
     this.session.set('run', 0);
   },
-  db: void 0,
-  fileToUrl: function (file) {
-    return chrome.runtime.getURL(file);
-  }
-};
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-  Notification.welcome();
-});
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.iframe) {
-    var data = request.iframe;
-
+  onWorker: function (e) {
+    console.log(e.data);
+  },
+  onClickedIcon: function (tab) {
+    Notification.welcome();
+  },
+  onMessage: function (req, sender, sendRes) {
+    if (req.iframe) {
+      this.onMessageFromIframe(req.iframe);
+    }
+  },
+  onMessageFromIframe: function (data) {
     var price = service.moneyToNumber(data.price);
     var cmp_price = service.moneyToNumber(data.cmp_price);
 
@@ -48,9 +42,25 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         Notification.thanks();
       });
 
-      service.db.put({_id: Utils.getStrId(), type: 'log', log: data.url});
+      service.db.put({
+        _id: Utils.getStrId(),
+        type: 'log',
+        log: data.url
+      });
     }
+  },
+  navigate: function (url) {
+    chrome.tabs.create({url: url});
+  },
+  moneyToNumber: function (str) {
+    return parseFloat(str.indexOf(DEFAULT_CURRENCY) > -1 ? str.split('.').join('') : str);
+  },
+  fileToUrl: function (file) {
+    return chrome.runtime.getURL(file);
+  },
+  getBlobScriptUrl: function (source) {
+    return window['URL']['createObjectURL'](new Blob([source], {type: 'text/javascript'}));
   }
-});
+};
 
 service.init();
